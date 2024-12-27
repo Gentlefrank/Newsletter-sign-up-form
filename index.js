d688b8f6507b46b9c8e134def791ccd9795d4586
apple <apple@apples-MacBook-Pro.local>
2024-12-27 07:44:28 +0100

    work done

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
 
 const getForm = document.querySelector("form")
 const getEmail= document.getElementById("email")
-let submitErr= true
+const getSubmitBtn=document.querySelector(".suscribe-btn")
+const getError= document.getElementById("errorMsg")
+//get div containers
+const getSignupDiv= document.querySelector(".signup")
+const getSuccessDiv= document.querySelector(".success")
+const getDismissBtn= document.querySelector(".dismiss-btn")
+
 let emailValidation= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ 
+let theError= ""
+ 
+getEmail.addEventListener('input', function(){
+    if (getEmail.value===""){
+        
+        theError= "email cannot be empty"
+        getSubmitBtn.style.opacity = "0.7"
+        getSubmitBtn.disabled = true
+        getEmail.style.border= "2px solid red"
+         
+        getError.textContent= `${theError}`
+    }else if(!emailValidation.test(getEmail.value)){
+        
+        theError= "Please enter a correct email format"
+        getSubmitBtn.style.opacity = "0.7"
+        getSubmitBtn.disabled = true
+          getError.textContent= `${theError}`
+    }else if(emailValidation.test(getEmail.value)){
+        getEmail.style.border= "2px solid green"
+        getSubmitBtn.disabled = false
+        getSubmitBtn.style.opacity = "1"
+        theError= ""
+        getError.textContent= `${theError}`
+    }
+})
 
 const handleSubmit =(e)=> {
     e.preventDefault();
     console.log(getEmail.value)
-    if (!emailValidation.test(getEmail.value)){
-        console.log("incorrect email pattern")
+   
+    if (getEmail.value!=="" && emailValidation.test(getEmail.value)){
+        getSignupDiv.style.display = "none"
+        getSuccessDiv.style.display = "flex"
+        console.log("here we are")
+        console.log(getSuccessDiv)
+        console.log(getSuccessDiv.offsetHeight > 0); 
     } else{
-        submitErr= true
-        console.log("good ")
+        theError= "Something's not right. Please cross-check your email"
+        getError.textContent= `${theError}`
+        console.log("Cross-check your information")
     }
 }
 
 getForm.addEventListener("submit", handleSubmit)
+
+getDismissBtn.addEventListener('click', function(){
+    getSignupDiv.style.display = "flex"
+    getSuccessDiv.style.display = "none"
+})
